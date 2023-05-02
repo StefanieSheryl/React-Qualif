037568018391685e11530edae65de013b955d9cf
Stefanie Sheryl <teresa.sheryl27@gmail.com>
2023-05-02 08:30:52 +0700

    Sixth Commit

--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -10,15 +10,15 @@ export default function NavBar(){
 
     return(
         <div className={`fixed bottom-0 ${theme.background5} w-full`}>
-            <ul className="flex flex-row h-20 items-center justify-center space-x-20">
+            <ul className="flex flex-row h-20 items-center justify-center">
                 <li >
-                <Link to={"/search"}><button><IoSearchOutline color="white"/></button></Link>
+                <Link to={"/search"}><button className="flex items-center justify-center w-20 h-20"><IoSearchOutline color="white"/></button></Link>
                 </li>
                 <li>
-                <Link to={"/home"}><button><HiOutlineHome color="white"/></button></Link>
+                <Link to={"/home"}><button className="flex items-center justify-center w-20 h-20"><HiOutlineHome color="white"/></button></Link>
                 </li>
                 <li>
-                <Link to={"/fav"}><button><AiOutlineHeart color="white"/></button></Link>
+                <Link to={"/fav"}><button  className="flex items-center justify-center w-20 h-20"><AiOutlineHeart color="white"/></button></Link>
                 </li>
             </ul>
     </div>
